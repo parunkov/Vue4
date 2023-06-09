df5a4b731050f38c9ca99fb9ca2fb9479d518e53
parunkov <parunkov@gmail.com>
2023-06-09 16:24:34 +0500

    summ

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -2,9 +2,24 @@
 import CartItem from '@/components/CartItem.vue';
 import { useStore } from 'vuex';
 import { key } from '@/store';
+import { ref, Ref } from 'vue';
 
 const store = useStore(key);
 store.state.cart;
+
+// console.log(store.state.cart);
+const currentSumm: number = store.state.cart.reduce(
+  (summ, item) => Math.round((summ + item.price * item.quantity) * 100) / 100,
+  0,
+);
+const totalSumm: Ref<number> = ref(currentSumm);
+const onItemClick: () => void = () => {
+  const summ = store.state.cart.reduce(
+    (summ, item) => Math.round((summ + item.price * item.quantity) * 100) / 100,
+    0,
+  );
+  totalSumm.value = summ;
+};
 </script>
 
 <template>
@@ -17,7 +32,14 @@ store.state.cart;
       <h2 class="headers__total">Total</h2>
       <div class="headers__trash"></div>
     </div>
-    <cart-item v-for="item in store.state.cart" :key="item.title" :data="item" />
+    <cart-item
+      v-for="item in store.state.cart"
+      :key="item.title"
+      :data="item"
+      @click="onItemClick"
+    />
+    <h2 class="summ">{{ 'Subtotal: $' + totalSumm }}</h2>
+    <button type="button" class="submit-button">Checkout</button>
   </div>
 </template>
 
@@ -49,6 +71,29 @@ h1 {
   }
   &__trash {
     width: 60px;
+  }
+}
+.summ {
+  text-align: right;
+  margin-top: 10px;
+  margin-bottom: 30px;
+}
+.submit-button {
+  display: block;
+  margin-left: auto;
+  margin-right: 0;
+  padding: 10px 60px;
+  border-radius: 5px;
+  background: black;
+  color: white;
+  font-size: 22px;
+  outline: none;
+  border: 1px solid black;
+  cursor: pointer;
+  &:hover {
+    color: black;
+    background: white;
+    border: 1px solid black;
   }
 }
 </style>
