9605413e7359d5329f55d94fc2cc0b2f3f0ca894
parunkov <parunkov@gmail.com>
2023-06-09 14:04:45 +0500

    quantity creating

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
 import { defineProps } from 'vue';
 import { CartItem } from '@/types/types';
-import { ref } from 'vue';
+// import { ref } from 'vue';
+import { useStore } from 'vuex';
+import { key } from '@/store';
+import { up, down } from '@/assets/images';
 
+const store = useStore(key);
+store.state.cart;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const props = defineProps<{
   data: CartItem;
 }>();
-
-// console.log(22222);
-
-// console.log(ref);
-
-// const cardData: any = ref(props.data);
-// console.log('🚀 ~ file: CartItem.vue:11 ~ cardData:', cardData);
-// const total: number = props.value.data.price;
-// console.log('🚀 ~ file: CartItem.vue:10 ~ total:', total);
+console.log(store);
 </script>
 
 <template>
@@ -22,7 +21,11 @@ const props = defineProps<{
     <img :src="require(`@/assets${data.image}`)" class="cart-item__image" />
     <h3 class="cart-item__title">{{ `${data.brandName} / ${data.title}` }}</h3>
     <div class="cart-item__price">{{ '$' + data.price }}</div>
-    <div class="cart-item__quantity">{{ data.quantity }}</div>
+    <div class="cart-item__quantity-wrapper">
+      <div class="cart-item__quantity">{{ data.quantity }}</div>
+      <div type="button" class="cart-item__plus" v-html="up"></div>
+      <div type="button" class="cart-item__minus" v-html="down"></div>
+    </div>
     <div class="cart-item__total">{{ '$' + data.price * data.quantity }}</div>
     <div class="cart-item__remove-button"></div>
   </div>
@@ -44,9 +47,29 @@ const props = defineProps<{
     padding-left: 10px;
     padding-right: 10px;
   }
-  &__quantity {
-    padding-left: 10px;
-    padding-right: 10px;
+  &__quantity-wrapper {
+    position: relative;
+    padding: 10px;
+    padding-right: 30px;
+    border: 1px solid black;
+    border-radius: 5px;
+  }
+  &__plus,
+  &__minus {
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    border: none;
+    background: transparent;
+    padding: 0;
+    right: 0;
+    cursor: pointer;
+  }
+  &__plus {
+    top: 0;
+  }
+  &__minus {
+    bottom: 0;
   }
   &__total {
     padding-left: 10px;
